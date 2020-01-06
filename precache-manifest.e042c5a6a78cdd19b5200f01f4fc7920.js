64d04fa7a0aa67a838bd7757a6956d7a693c6d6b
Souvik Srimani <55970375+Rotten-Banana@users.noreply.github.com>
2020-01-06 22:28:53 +0530

    Updates

---
+++ precache-manifest.e042c5a6a78cdd19b5200f01f4fc7920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21653d2fc2b4a3da7a0ff3c2e6aef18",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "018bdfd34b172b7fade5",
+    "url": "/pokedex/static/css/main.51cded00.chunk.css"
+  },
+  {
+    "revision": "08b77da91f7ebcf7f3da",
+    "url": "/pokedex/static/js/2.3f48e1d7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pokedex/static/js/2.3f48e1d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "018bdfd34b172b7fade5",
+    "url": "/pokedex/static/js/main.283f0978.chunk.js"
+  },
+  {
+    "revision": "3aa27ede24b403eac752",
+    "url": "/pokedex/static/js/runtime-main.8eb4c1ce.js"
+  }
+]);
